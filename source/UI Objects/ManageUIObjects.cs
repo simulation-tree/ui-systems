6295967833b24d10120d1b75ff599abcc8607546
Phill <philldamaskin@gmail.com>
2025-05-01 12:34:39 -0400

    work on loading ui from file

---
+++ source/UI Objects/ManageUIObjects.cs
@@ -0,0 +1,117 @@
+﻿using Collections.Generic;
+using Data.Messages;
+using Serialization.JSON;
+using Simulation;
+using System;
+using UI.Components;
+using Unmanaged;
+using Worlds;
+
+namespace UI.Systems
+{
+    public readonly partial struct ManageUIObjects : ISystem
+    {
+        private readonly Dictionary<World, Operation> operations;
+        private readonly Dictionary<Entity, uint> sceneObjects;
+
+        public ManageUIObjects()
+        {
+            operations = new();
+            sceneObjects = new();
+        }
+
+        readonly void IDisposable.Dispose()
+        {
+            sceneObjects.Dispose();
+
+            foreach (Operation operation in operations.Values)
+            {
+                operation.Dispose();
+            }
+
+            operations.Dispose();
+        }
+
+        void ISystem.Start(in SystemContext context, in World world)
+        {
+            ref Operation operation = ref operations.TryGetValue(world, out bool contains);
+            if (!contains)
+            {
+                operation = ref operations.Add(world);
+                operation = new();
+            }
+        }
+
+        void ISystem.Update(in SystemContext context, in World world, in TimeSpan delta)
+        {
+            ref Operation operation = ref operations[world];
+            CreateObjects(context, world, operation, sceneObjects, delta);
+
+            if (operation.Count > 0)
+            {
+                operation.Perform(world);
+                operation.Reset();
+            }
+        }
+
+        void ISystem.Finish(in SystemContext context, in World world)
+        {
+        }
+
+        private static void CreateObjects(SystemContext context, World world, Operation operation, Dictionary<Entity, uint> sceneObjects, TimeSpan delta)
+        {
+            Schema schema = world.Schema;
+            int uiObjectRequestComponent = schema.GetComponentType<IsUIObjectRequest>();
+            int uiObjectTag = schema.GetTagType<IsUIObject>();
+            foreach (Chunk chunk in world.Chunks)
+            {
+                Definition definition = chunk.Definition;
+                if (!definition.ContainsTag(uiObjectTag) && definition.ContainsComponent(uiObjectRequestComponent))
+                {
+                    ReadOnlySpan<uint> entities = chunk.Entities;
+                    ComponentEnumerator<IsUIObjectRequest> requestComponents = chunk.GetComponents<IsUIObjectRequest>(uiObjectRequestComponent);
+                    for (int i = 0; i < entities.Length; i++)
+                    {
+                        ref IsUIObjectRequest request = ref requestComponents[i];
+                        if (request.status == IsUIObjectRequest.Status.Submitted)
+                        {
+                            request.status = IsUIObjectRequest.Status.Loading;
+                        }
+
+                        if (request.status == IsUIObjectRequest.Status.Loading)
+                        {
+                            if (TryImportUIObject(context, world, request.address, sceneObjects, operation))
+                            {
+                                //yay
+                            }
+                            else
+                            {
+                                request.duration += delta;
+                                if (request.duration >= request.timeout)
+                                {
+                                    request.status = IsUIObjectRequest.Status.NotFound;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool TryImportUIObject(SystemContext context, World world, ASCIIText256 address, Dictionary<Entity, uint> sceneObjects, Operation operation)
+        {
+            LoadData message = new(world, address);
+            if (context.TryHandleMessage(ref message) != default)
+            {
+                if (message.TryConsume(out ByteReader data))
+                {
+                    JSONReader jsonReader = new(data);
+                    data.Dispose();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ source/UI Objects/UIScene.cs
@@ -0,0 +1,138 @@
+﻿using Collections.Generic;
+using System;
+using Types;
+using Unmanaged;
+
+namespace UI.Systems
+{
+    internal readonly struct UIScene : IDisposable
+    {
+        private readonly List<UIValue> values;
+
+        public readonly ReadOnlySpan<UIValue.Borrowed> Values => values.AsSpan().As<UIValue, UIValue.Borrowed>();
+
+        public UIScene()
+        {
+            values = new();
+        }
+
+        public readonly void Dispose()
+        {
+            foreach (UIValue value in values)
+            {
+                value.Dispose();
+            }
+
+            values.Dispose();
+        }
+
+        public readonly void Add<T>(ReadOnlySpan<char> name, T value) where T : unmanaged
+        {
+            values.Add(UIValue.Create(name, value));
+        }
+
+        public readonly void Add(ReadOnlySpan<char> name, ReadOnlySpan<char> value)
+        {
+            values.Add(new(name, value));
+        }
+
+        public readonly bool TryGet(ReadOnlySpan<char> name, out UIValue.Borrowed value)
+        {
+            Span<UIValue> nodes = this.values.AsSpan();
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i].Name.SequenceEqual(name))
+                {
+                    value = new(nodes[i]);
+                    return true;
+                }
+            }
+
+            value = default;
+            return false;
+        }
+    }
+
+    internal readonly struct UIValue : IDisposable
+    {
+        public readonly TypeMetadata type;
+
+        private readonly int nameLength;
+        private readonly int valueLength;
+        private readonly MemoryAddress allocation;
+
+        public readonly ReadOnlySpan<char> Name => allocation.GetSpan<char>(nameLength);
+
+        private UIValue(int nameLength, int valueLength, MemoryAddress allocation, TypeMetadata type)
+        {
+            this.nameLength = nameLength;
+            this.valueLength = valueLength;
+            this.allocation = allocation;
+            this.type = type;
+        }
+
+        public UIValue(ReadOnlySpan<char> name, ReadOnlySpan<char> text)
+        {
+            nameLength = name.Length;
+            valueLength = text.Length;
+            allocation = MemoryAddress.Allocate((nameLength + valueLength) * sizeof(char));
+            allocation.CopyFrom(name);
+            allocation.CopyFrom(text, nameLength * sizeof(char));
+            type = default;
+        }
+
+        public readonly void Dispose()
+        {
+            if (type.Is<UIValue>())
+            {
+                UIValue child = GetValue<UIValue>();
+                child.Dispose();
+            }
+
+            allocation.Dispose();
+        }
+
+        public readonly ReadOnlySpan<char> GetText()
+        {
+            return allocation.GetSpan<char>(nameLength);
+        }
+
+        public readonly ref T GetValue<T>() where T : unmanaged
+        {
+            return ref allocation.Read<T>(nameLength * sizeof(char));
+        }
+
+        public unsafe static UIValue Create<T>(ReadOnlySpan<char> name, T value) where T : unmanaged
+        {
+            int nameLength = name.Length;
+            int valueLength = sizeof(T);
+            MemoryAddress allocation = MemoryAddress.Allocate((nameLength * sizeof(char)) + valueLength);
+            allocation.CopyFrom(name);
+            allocation.Write(nameLength * sizeof(char), value);
+            TypeMetadata type = TypeMetadata.GetOrRegister<T>();
+            return new UIValue(nameLength, -1, allocation, type);
+        }
+
+        public readonly struct Borrowed
+        {
+            private readonly UIValue node;
+
+            public readonly TypeMetadata Type => node.type;
+
+            public Borrowed(UIValue node)
+            {
+                this.node = node;
+            }
+
+            public readonly ReadOnlySpan<char> GetText()
+            {
+                return node.GetText();
+            }
+
+            public readonly ref T GetValue<T>() where T : unmanaged
+            {
+                return ref node.GetValue<T>();
+            }
+        }
+    }
+}
